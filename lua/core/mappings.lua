47625c2fe0c8930dd6fe0d7a81fcfa9a9587844c
Laban Oleg <OlegLaban@yandex.by>
2025-12-09 15:16:42 +0300

    add bloking for arrows

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -4,3 +4,11 @@ vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = "Show diagn
 
 vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, { desc = "Code action" })
 
+local opts = { noremap = true, silent = true }
+for _, mode in ipairs({'n','i','v'}) do
+  vim.keymap.set(mode, '<Up>', '<Cmd>echo "Use - k"<CR>', opts)
+  vim.keymap.set(mode, '<Down>', '<Cmd>echo "Use - j"<CR>', opts)
+  vim.keymap.set(mode, '<Left>', '<Cmd>echo "Use - h"<CR>', opts)
+  vim.keymap.set(mode, '<Right>', '<Cmd>echo "Use - l"<CR>', opts)
+end
+
